6e74f3517733444116a1c8032c8907bd69cacef9
Theophilus <93491747+nwherman1724@users.noreply.github.com>
2023-04-08 13:21:06 -0400

    Building models

--- models/users.js
+++ models/users.js
@@ -36,3 +36,9 @@ const userSchema = new Schema(
    id: false,
  }
 );
+
+userSchema.virtual("friendCount").get(function () {
+   return this.friends.length;
+ });
+ const User = model('User', userSchema);
+ module.exports = User;
