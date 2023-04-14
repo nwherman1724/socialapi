9967860e605c58bec51f0f55300ba65dfad4eb31
Theophilus <93491747+nwherman1724@users.noreply.github.com>
2023-04-14 11:29:33 -0400

    debuggging npm start error

--- models/thoughts.js
+++ models/thoughts.js
@@ -1,32 +1,5 @@
 const { Schema, model } = require('mongoose');
 
-const thoughtSchema = new Schema(
-    {
-        thoughtText: {
-            type: String,
-            required: true,
-            minlength: 1,
-            maxlength: 280
-        },
-        createdAt: {
-            type: Date,
-            default: Date.now,
-        },
-        username: {
-            type: String,
-            required: true,
-        },
-        reactions: [reactionSchema],
-    },
-    {
-        toJSON: {
-            virtuals: true,
-            getters: true,
-        },
-        id: false,
-    }
-);
-
 const reactionSchema = new Schema(
     {
         reactionId : {
@@ -60,6 +33,33 @@ thoughtSchema.virtual('reactionCount')
 .get(function() {
     return this.reactions.length;
 })
+
+const thoughtSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            minlength: 1,
+            maxlength: 280
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+        },
+        username: {
+            type: String,
+            required: true,
+        },
+        reactions: [reactionSchema],
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true,
+        },
+        id: false,
+    }
+);
 
 const Thought = model('Thought', thoughtSchema);
 module.exports = Thought;
